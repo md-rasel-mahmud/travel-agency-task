d85c7156b7cda05e82f2eb3b1caea9e305303277
md-rasel-mahmud <md.rasel.mahmud.mail@gmail.com>
2024-05-13 19:08:28 +0600

    Navbar: make menu items toggle in mobile view

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,20 @@
 import { Link } from "react-router-dom";
 import { navMenuItems } from "../helpers/nav-menu-items";
+import { IoNotificationsOutline } from "react-icons/io5";
+import { FaBars } from "react-icons/fa";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [isToggleMenu, setIsToggleMenu] = useState(true);
+
   return (
     <nav className="bg-gray-800 border-gray-700 sticky top-0 z-50">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto py-4">
-        <div className="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
+        <div
+          className={`${
+            isToggleMenu ? "hidden" : ""
+          } w-full md:block md:w-auto" id="navbar-solid-bg`}
+        >
           <ul className="flex flex-col font-medium mt-4 rounded-lg  md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0  bg-gray-800 md:bg-transparent border-gray-700">
             {navMenuItems.map((item, index) => (
               <li key={index}>
@@ -20,36 +29,26 @@ const Navbar = () => {
           </ul>
         </div>
 
-        <a href="#" className="flex items-center space-x-3">
-          <img
-            src="https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fHww"
-            className="h-8 object-cover rounded-full w-8"
-            alt="Navigation Logo"
-          />
-          <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">
-            Task
-          </span>
-        </a>
+        <div className="flex p-2 flex-row-reverse md:flex-row gap-2 items-center text-gray-400">
+          <button>
+            <IoNotificationsOutline size={20} />
+          </button>
+
+          <button>
+            <img
+              src="https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fHww"
+              className="h-8 object-cover rounded-full w-8"
+              alt="Navigation Logo"
+            />
+          </button>
+        </div>
         <button
           type="button"
-          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm  rounded-lg md:hidden  focus:outline-none focus:ring-2 text-gray-400 hover:bg-gray-700 focus:ring-gray-600"
+          onClick={() => setIsToggleMenu(!isToggleMenu)}
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm rounded-lg md:hidden  focus:outline-none focus:ring-2 text-gray-400 hover:bg-gray-700 focus:ring-gray-600"
         >
           <span className="sr-only">Open main menu</span>
-          <svg
-            className="w-5 h-5"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 17 14"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M1 1h15M1 7h15M1 13h15"
-            />
-          </svg>
+          <FaBars size={20} />
         </button>
       </div>
     </nav>
