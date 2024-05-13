0f8b58336815047619c30a6adba058976c82b73c
md-rasel-mahmud <md.rasel.mahmud.mail@gmail.com>
2024-05-13 11:30:00 +0600

    MasterPrice: Parse the data and show message to ui

--- src/pages/MasterPrice.jsx
+++ src/pages/MasterPrice.jsx
@@ -1,9 +1,15 @@
+import { useEffect } from "react";
 import ComponentHeading from "../components/ComponentHeading";
 import FilterActionBar from "../components/master-price/FilterActionBar";
 import FilterInputBar from "../components/master-price/FilterInputBar";
 import TopButtonGroup from "../components/master-price/TopButtonGroup";
+import { useState } from "react";
 
 const MasterPrice = () => {
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+
+  // HANDLE FORM SUBMIT
   const handleSubmit = (e) => {
     e.preventDefault();
 
@@ -25,16 +31,51 @@ const MasterPrice = () => {
     console.log(data);
   };
 
+  useEffect(() => {
+    // FETCH DATA FROM TEXT FILE IN DATA FOLDER AND PARSE IT TO JSON
+    const fetchAndParseData = async () => {
+      try {
+        setIsLoading(true);
+        const response = await fetch("/data/LHR_CDG_ADT1_TYPE_1.txt");
+        const jsonData = await response.json();
+
+        setData(jsonData);
+        setIsLoading(false);
+      } catch (error) {
+        setIsLoading(false);
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    // CALL FUNCTION
+    fetchAndParseData();
+  }, []);
+
+  console.log(data);
+
   return (
     <div>
       <ComponentHeading heading="Master Price" />
-
+      {/* TOP BUTTON GROUP */}
       <TopButtonGroup />
-
+      {/* FILTER BAR */}
       <form onSubmit={handleSubmit}>
         <FilterInputBar />
         <FilterActionBar />
       </form>
+      {/* LOADING */}
+      {isLoading && (
+        <h3 className="text-xl font-bold inline py-1 animate-pulse">
+          Loading...
+        </h3>
+      )}
+
+      {/* DATA */}
+      {data?.code == "200" && (
+        <div>
+          <ComponentHeading heading={data.message} />
+        </div>
+      )}
     </div>
   );
 };
